f2ff2bd7706e2bd501bb5ff897973d15089855bf
henrymbuguak <henrymbuguak@gmail.com>
2017-08-09 22:45:35 +0300

    User authentication

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
-from . import views
+from account import views as account_views
+from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name='login'),
+    url(r'^$', account_views.dashboard, name='dashboard'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$',views.logout, name='logout'),
+    url(r'^logout-then-login/$',views.logout_then_login, name='logout-then-login'),
+    #url(r'^login/$', views.user_login, name='login'),
+
 ]
