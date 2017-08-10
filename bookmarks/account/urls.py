cf4f05af1f45b301a6fe7168072ef8f18c4221bc
henrymbuguak <henrymbuguak@gmail.com>
2017-08-10 14:05:37 +0300

    Password reset authentication

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -9,4 +9,15 @@ urlpatterns = [
     url(r'^logout-then-login/$',views.logout_then_login, name='logout-then-login'),
     #url(r'^login/$', views.user_login, name='login'),
 
+    #password change urls
+    url(r'^password-change/$', views.password_change, name='password_change'),
+    url(r'^password-change/done/$', views.password_change_done, name='password_change_done'),
+
+    #reset password urls
+    url(r'^password-reset/$', views.password_reset, name='password_reset'),
+    url(r'^password-reset/done/$', views.password_reset, name='password_reset_done'),
+    url(r'^password-reset/confirm/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/$', views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^password-reset/complete/$', views.password_reset_complete, name='password_reset_complete'),
+
+
 ]
--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -126,3 +126,12 @@ from django.core.urlresolvers import reverse_lazy
 LOGIN_REDIRECT_URL = reverse_lazy('dashboard')
 LOGIN_URL = reverse_lazy('login')
 LOGOUT_URL = reverse_lazy('logout')
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+SITE_ID = 1
