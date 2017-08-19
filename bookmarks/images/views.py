e9493a0385a34dce803bfb4df1f99ed44c072f1c
henrymbuguak <henrymbuguak@gmail.com>
2017-08-19 15:35:43 +0300

    Ajax Pagination

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_POST
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from .models import Image
 from common.decorators import ajax_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 @login_required
 def image_create(request):
@@ -46,3 +48,21 @@ def image_like(request):
         except:
             pass
     return JsonResponse({'status':'ok'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if request.is_ajax():
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if request.is_ajax():
+        return(request,'images/image/list_ajax.html',{'section':'images', 'images':images})
+    return(request,'images/image/list.html',{'section':'images', 'images':images})
